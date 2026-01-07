480b56541ea21288239a0690a36221cbf3c8127c
ia0n <ia0n@protonmail.com>
2026-01-07 13:45:01 +0100

    Changed source candidate search view. Sdded video. Changed workflow of the summary generation with a test variable.

---
+++ client/src/components/CandidateSelector.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => {
+    initFlowbite();
+})
+
+const props = defineProps({
+    
+})
+</script>
+
+<template>
+    
+    
+
+
+</template>
